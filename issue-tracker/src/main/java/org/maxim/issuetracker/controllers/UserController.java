e3299ebb983e2b5a26a6396fbb86a637bada5da2
Maxim Potapenko <maxim131994@gmail.com>
2015-08-04 16:23:34 +0300

    Added attributes: project, activities.

--- issue-tracker/src/main/java/org/maxim/issuetracker/controllers/UserController.java
+++ issue-tracker/src/main/java/org/maxim/issuetracker/controllers/UserController.java
@@ -1,7 +1,9 @@
 package org.maxim.issuetracker.controllers;
 
+import org.maxim.issuetracker.domain.Activity;
 import org.maxim.issuetracker.domain.Employee;
 import org.maxim.issuetracker.security.SecurityConstants;
+import org.maxim.issuetracker.service.interfaces.ActivityService;
 import org.maxim.issuetracker.service.interfaces.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
@@ -11,6 +13,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
+
 @Controller
 @RequestMapping(value = "/user")
 public class UserController {
@@ -18,14 +22,20 @@ public class UserController {
     @Autowired
     private EmployeeService employeeService;
 
+    @Autowired
+    private ActivityService activityService;
+
     @Secured(value = SecurityConstants.ROLE_USER)
     @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
     public String showDashboard(Model model) {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         Employee user = employeeService.findByLogin(username);
+        List<Activity> lastActivities = activityService.list();
 
-        model.addAttribute("msg", username + " default dashboard for user");
         model.addAttribute("userinfo", user.getFirstName() + " " + user.getLastName());
+        model.addAttribute("projects", user.getMembers());
+        model.addAttribute("activities", lastActivities);
+
         return "dashboard";
     }
 
