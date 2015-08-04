15e325e65dc2ae9550f732baf5fa230e31bf9234
Maxim Potapenko <maxim131994@gmail.com>
2015-08-04 16:15:57 +0300

    Updated mapping for admin controller.

--- issue-tracker/src/main/java/org/maxim/issuetracker/controllers/AdminController.java
+++ issue-tracker/src/main/java/org/maxim/issuetracker/controllers/AdminController.java
@@ -4,7 +4,6 @@ import org.maxim.issuetracker.security.SecurityConstants;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,10 +11,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "/admin")
 public class AdminController {
 
-    @Secured({SecurityConstants.ROLE_ADMIN})
-    @RequestMapping(value = "/{username}/dashboard", method = RequestMethod.GET)
-    public String showAdmin(@PathVariable String username, Model model) {
-        model.addAttribute("msg", username + " default dashboard for admin");
+    @Secured(SecurityConstants.ROLE_ADMIN)
+    @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
+    public String showAdmin(Model model) {
         return "dashboard";
     }
 
