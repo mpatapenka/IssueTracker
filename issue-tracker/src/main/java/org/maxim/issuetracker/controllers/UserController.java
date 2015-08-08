5ae4155c86992847c71c48292160178dc829de0d
Maxim Potapenko <maxim131994@gmail.com>
2015-08-08 18:26:21 +0300

    Reworked method for return last activity bundle.

--- issue-tracker/src/main/java/org/maxim/issuetracker/controllers/UserController.java
+++ issue-tracker/src/main/java/org/maxim/issuetracker/controllers/UserController.java
@@ -1,7 +1,11 @@
 package org.maxim.issuetracker.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.maxim.issuetracker.domain.Activity;
+import org.maxim.issuetracker.domain.Assigment;
 import org.maxim.issuetracker.domain.Employee;
+import org.maxim.issuetracker.domain.Member;
 import org.maxim.issuetracker.security.SecurityConstants;
 import org.maxim.issuetracker.service.interfaces.ActivityService;
 import org.maxim.issuetracker.service.interfaces.EmployeeService;
@@ -12,8 +16,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
+import java.util.*;
 
 @Controller
 @RequestMapping(value = "/user")
@@ -29,14 +35,49 @@ public class UserController {
     @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
     public String showDashboard(Model model) {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        Employee user = employeeService.findByLogin(username);
-        List<Activity> lastActivities = activityService.list();
 
-        model.addAttribute("userinfo", user.getFirstName() + " " + user.getLastName());
-        model.addAttribute("projects", user.getMembers());
-        model.addAttribute("activities", lastActivities);
+        Employee user = employeeService.findByLogin(username);
+        Set<Member> members = user.getMembers();
+        List<Assigment> assignToMe = new ArrayList<>();
+        for (Member member : members) {
+            assignToMe.addAll(member.getAssigments());
+        }
+        int withoutOffset = 0;
+        List<Activity> lastActivities = activityService.listLast(withoutOffset);
+
+        model.addAttribute("userFullName", user.getFirstName() + " " + user.getLastName());
+        model.addAttribute("members", members);
+        model.addAttribute("lastActivities", lastActivities);
+        model.addAttribute("assignToMe", assignToMe);
 
         return "dashboard";
+    }
+
+    @Secured(value = SecurityConstants.ROLE_USER)
+    @RequestMapping(value = "/dashboard/activity", method = RequestMethod.GET)
+    public @ResponseBody String getActivities(@RequestParam int offset) {
+        List<Activity> activities = activityService.listLast(offset);
+
+        List<Object> activityData = new ArrayList<>();
+        for (Activity activity : activities) {
+            Employee employee = activity.getMember().getEmployee();
+            Map<String, Object> activityInfo = new HashMap<>();
+
+            activityInfo.put("name", employee.getFirstName() + " " + employee.getLastName());
+            activityInfo.put("date", activity.getDate());
+            activityInfo.put("comment", activity.getComment());
+
+            activityData.add(activityInfo);
+        }
+
+        String jsonString = "";
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            jsonString = mapper.writeValueAsString(activityData);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return jsonString;
     }
 
 }
