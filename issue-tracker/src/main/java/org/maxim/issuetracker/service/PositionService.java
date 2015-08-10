693452a71f4361513179fc32e34b41200b687855
Maxim Potapenko <maxim131994@gmail.com>
2015-08-10 23:02:29 +0300

    Added position service and impl.

---
+++ issue-tracker/src/main/java/org/maxim/issuetracker/service/PositionService.java
@@ -0,0 +1,13 @@
+package org.maxim.issuetracker.service;
+
+import org.maxim.issuetracker.domain.Position;
+
+import java.util.List;
+
+public interface PositionService {
+
+    Position get(int id);
+
+    List<Position> list();
+
+}
---
+++ issue-tracker/src/main/java/org/maxim/issuetracker/service/impls/PositionServiceImpl.java
@@ -0,0 +1,30 @@
+package org.maxim.issuetracker.service.impls;
+
+import org.maxim.issuetracker.dao.PositionDAO;
+import org.maxim.issuetracker.domain.Position;
+import org.maxim.issuetracker.service.PositionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class PositionServiceImpl implements PositionService {
+
+    @Autowired
+    private PositionDAO positionDAO;
+
+    @Override
+    @Transactional
+    public Position get(int id) {
+        return positionDAO.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public List<Position> list() {
+        return positionDAO.list();
+    }
+
+}
