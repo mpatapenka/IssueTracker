1326e87dc81d09bd6b7ba0d09da4ae7908e582e3
Maxim Potapenko <maxim131994@gmail.com>
2015-08-12 13:31:37 +0300

    Added member service and implementation.

---
+++ issue-tracker/src/main/java/org/maxim/issuetracker/service/MemberService.java
@@ -0,0 +1,11 @@
+package org.maxim.issuetracker.service;
+
+import org.maxim.issuetracker.domain.Member;
+
+import java.util.List;
+
+public interface MemberService {
+
+    List<Member> list();
+
+}
---
+++ issue-tracker/src/main/java/org/maxim/issuetracker/service/impls/MemberServiceImpl.java
@@ -0,0 +1,24 @@
+package org.maxim.issuetracker.service.impls;
+
+import org.maxim.issuetracker.dao.MemberDAO;
+import org.maxim.issuetracker.domain.Member;
+import org.maxim.issuetracker.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class MemberServiceImpl implements MemberService {
+
+    @Autowired
+    private MemberDAO memberDAO;
+
+    @Override
+    @Transactional
+    public List<Member> list() {
+        return memberDAO.list();
+    }
+
+}
