ecdb1371632f1c427d176822fb85a2c1e0b979b0
Maxim Potapenko <maxim131994@gmail.com>
2015-08-12 13:30:38 +0300

    Optimize service register method.

--- issue-tracker/src/main/java/org/maxim/issuetracker/service/impls/EmployeeServiceImpl.java
+++ issue-tracker/src/main/java/org/maxim/issuetracker/service/impls/EmployeeServiceImpl.java
@@ -7,7 +7,6 @@ import org.maxim.issuetracker.domain.Position;
 import org.maxim.issuetracker.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -20,16 +19,15 @@ public class EmployeeServiceImpl implements EmployeeService {
     private PositionDAO positionDAO;
 
     @Override
-    @Transactional(isolation = Isolation.SERIALIZABLE)
-    public boolean register(Employee employee) {
+    @Transactional
+    public synchronized void register(Employee employee) {
         Employee check = findByLogin(employee.getLogin());
         if (check != null) {
-            return false;
+            throw new IllegalArgumentException("User with username '" + employee.getLogin() + "' already exist.");
         }
         Position position = positionDAO.findById(employee.getPosition().getId());
         employee.setPosition(position);
         employeeDAO.save(employee);
-        return true;
     }
 
     @Override
