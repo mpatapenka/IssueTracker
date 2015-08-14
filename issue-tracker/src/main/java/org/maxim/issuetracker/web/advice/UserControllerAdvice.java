39d7c224395e9e1df9effadc9268f1ea6e37f7fd
Maxim Potapenko <maxim131994@gmail.com>
2015-08-14 22:26:33 +0300

    Refactory user controller advice.

--- issue-tracker/src/main/java/org/maxim/issuetracker/web/advice/UserControllerAdvice.java
+++ issue-tracker/src/main/java/org/maxim/issuetracker/web/advice/UserControllerAdvice.java
@@ -1,15 +1,13 @@
 package org.maxim.issuetracker.web.advice;
 
 import org.maxim.issuetracker.domain.Employee;
-import org.maxim.issuetracker.domain.Member;
+import org.maxim.issuetracker.domain.Task;
 import org.maxim.issuetracker.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
-
-import java.util.Set;
 
 @ControllerAdvice
 public class UserControllerAdvice {
@@ -22,10 +20,9 @@ public class UserControllerAdvice {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         Employee user = employeeService.findByLogin(username);
         if (user != null) {
-            Set<Member> members = user.getMembers();
-
-            model.addAttribute("members", members);
-            model.addAttribute("userFullName", user.getFirstName() + " " + user.getLastName());
+            model.addAttribute("task", new Task());
+            model.addAttribute("members", user.getMembers());
+            model.addAttribute("user", user.getFirstName() + " " + user.getLastName());
         }
     }
 
