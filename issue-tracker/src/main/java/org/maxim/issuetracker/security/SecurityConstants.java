a2b81b07739517bf95abebfeb481039b41917e8e
Maxim Potapenko <maxim131994@gmail.com>
2015-08-14 22:39:19 +0300

    Added new constant for lead role.

--- issue-tracker/src/main/java/org/maxim/issuetracker/security/SecurityConstants.java
+++ issue-tracker/src/main/java/org/maxim/issuetracker/security/SecurityConstants.java
@@ -3,6 +3,7 @@ package org.maxim.issuetracker.security;
 public final class SecurityConstants {
 
     public static final String ROLE_USER = "ROLE_USER";
+    public static final String ROLE_LEAD = "ROLE_LEAD";
     public static final String ROLE_ADMIN = "ROLE_ADMIN";
     public static final String ROLE_ANONYMOUS = "ROLE_ANONYMOUS";
 
