8b76f38669faa848659060b4f1d27fd06c9d90d5
Maxim Potapenko <maxim131994@gmail.com>
2015-08-15 19:32:15 +0300

    Added new attributes for user (in controller advice).

--- issue-tracker/src/main/java/org/maxim/issuetracker/web/advice/UserControllerAdvice.java
+++ issue-tracker/src/main/java/org/maxim/issuetracker/web/advice/UserControllerAdvice.java
@@ -1,8 +1,9 @@
 package org.maxim.issuetracker.web.advice;
 
+import org.maxim.issuetracker.domain.Assigment;
 import org.maxim.issuetracker.domain.Employee;
-import org.maxim.issuetracker.domain.Task;
 import org.maxim.issuetracker.service.EmployeeService;
+import org.maxim.issuetracker.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
@@ -15,15 +16,23 @@ public class UserControllerAdvice {
     @Autowired
     private EmployeeService employeeService;
 
+    @Autowired
+    private ProjectService projectService;
+
     @ModelAttribute
     public void addUserDefaultInfoToModel(Model model) {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         Employee user = employeeService.findByLogin(username);
         if (user != null) {
-            model.addAttribute("task", new Task());
             model.addAttribute("members", user.getMembers());
             model.addAttribute("user", user.getFirstName() + " " + user.getLastName());
         }
+    }
+
+    @ModelAttribute
+    public void addFormData(Model model) {
+        model.addAttribute("newAssign", new Assigment());
+        model.addAttribute("allProjects", projectService.list());
     }
 
 }
