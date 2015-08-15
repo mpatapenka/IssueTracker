0bf2aa0bf90eb048f83f685de328cd6a59474aa2
Maxim Potapenko <maxim131994@gmail.com>
2015-08-15 19:26:05 +0300

    Added method for creating json string with project members.

--- issue-tracker/src/main/java/org/maxim/issuetracker/service/ProjectService.java
+++ issue-tracker/src/main/java/org/maxim/issuetracker/service/ProjectService.java
@@ -12,4 +12,6 @@ public interface ProjectService {
 
     List<Project> list();
 
+    String getProjectMembersWithJson(Project project);
+
 }
--- issue-tracker/src/main/java/org/maxim/issuetracker/service/impls/ProjectServiceImpl.java
+++ issue-tracker/src/main/java/org/maxim/issuetracker/service/impls/ProjectServiceImpl.java
@@ -1,13 +1,17 @@
 package org.maxim.issuetracker.service.impls;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.maxim.issuetracker.dao.ProjectDAO;
+import org.maxim.issuetracker.domain.Employee;
+import org.maxim.issuetracker.domain.Member;
 import org.maxim.issuetracker.domain.Project;
 import org.maxim.issuetracker.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.util.*;
 
 @Service
 public class ProjectServiceImpl implements ProjectService {
@@ -31,6 +35,33 @@ public class ProjectServiceImpl implements ProjectService {
     @Transactional
     public List<Project> list() {
         return projectDAO.list();
+    }
+
+    @Override
+    public String getProjectMembersWithJson(Project project) {
+        if (project == null) {
+            return "";
+        }
+
+        Set<Member> members = project.getMembers();
+
+        List<Object> employeeData = new ArrayList<>();
+        for (Member member : members) {
+            Employee employee = member.getEmployee();
+            Map<String, Object> employeeInfo = new HashMap<>();
+
+            employeeInfo.put("name", employee.getFirstName() + " " + employee.getLastName());
+            employeeInfo.put("id", member.getId());
+
+            employeeData.add(employeeInfo);
+        }
+
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.writeValueAsString(employeeData);
+        } catch (JsonProcessingException ignore) {
+            return "";
+        }
     }
 
 }
