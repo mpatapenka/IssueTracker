3054c70cb032d48386b04a58addb6cc89f0ca7fe
Maxim Potapenko <maxim131994@gmail.com>
2015-08-18 16:03:55 +0300

    For role created new equals & hashcode.

--- issue-tracker/src/main/java/org/maxim/issuetracker/domain/Role.java
+++ issue-tracker/src/main/java/org/maxim/issuetracker/domain/Role.java
@@ -58,15 +58,15 @@ public class Role implements Serializable {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+
         Role role = (Role) o;
-        if (id != role.id) return false;
-        if (name != null ? !name.equals(role.name) : role.name != null) return false;
-        return !(members != null ? !members.equals(role.members) : role.members != null);
+
+        return !(name != null ? !name.equals(role.name) : role.name != null);
     }
 
     @Override
     public int hashCode() {
-        return id;
+        return name != null ? name.hashCode() : 0;
     }
 
     @Override
