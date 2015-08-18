3de9d56d7493eb2c9569b83dd05c80a3f5bfaf7d
Maxim Potapenko <maxim131994@gmail.com>
2015-08-18 19:02:23 +0300

    Added mapping constants.

---
+++ issue-tracker/src/main/java/org/maxim/issuetracker/web/constants/MappingConstants.java
@@ -0,0 +1,33 @@
+package org.maxim.issuetracker.web.constants;
+
+public final class MappingConstants {
+
+    public static final String REDIRECT = "redirect:";
+
+
+
+    public static final String ADMIN_PANEL = "/admin-panel";
+    public static final String REGISTER = "/register";
+    public static final String PROJECTS = "/projects";
+    public static final String ROOT = "/";
+    public static final String DASHBOARD = "/dashboard";
+    public static final String LOGIN = "/login";
+    public static final String ACTIVITY = "/activity";
+    public static final String EMPLOYEES = "/employees";
+    public static final String ISSUES = "/issues";
+
+
+
+    public static final String PAGE_ADMIN = "adminpage";
+    public static final String PAGE_REGISTER = "register";
+    public static final String PAGE_DASHBOARD = "dashboard";
+    public static final String PAGE_LOGIN = "login";
+    public static final String PAGE_PROJECTS = "projects";
+    public static final String PAGE_ISSUES = "issues";
+    public static final String PAGE_404 = "404";
+
+
+
+    private MappingConstants() { }
+
+}
