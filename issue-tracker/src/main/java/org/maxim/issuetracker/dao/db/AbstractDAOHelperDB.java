7e3384d0bf27630c63b3e7af489c9f143f190836
Maxim Potapenko <maxim131994@gmail.com>
2015-08-18 16:14:30 +0300

    Deleted unuser constructors, setters, getters from dao db helper.

--- issue-tracker/src/main/java/org/maxim/issuetracker/dao/db/AbstractDAOHelperDB.java
+++ issue-tracker/src/main/java/org/maxim/issuetracker/dao/db/AbstractDAOHelperDB.java
@@ -9,16 +9,6 @@ public abstract class AbstractDAOHelperDB {
     @Autowired
     private SessionFactory sessionFactory;
 
-    public AbstractDAOHelperDB() { }
-
-    public AbstractDAOHelperDB(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
-
-    public void setSessionFactory(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
-
     protected Session currentSession() {
         return sessionFactory.getCurrentSession();
     }
