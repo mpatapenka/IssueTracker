7713be182f1b85ad74f0a2a918d9feb6ee2bd1fa
Maxim Potapenko <maxim131994@gmail.com>
2015-08-18 19:02:44 +0300

    Added attribute constants.

---
+++ issue-tracker/src/main/java/org/maxim/issuetracker/web/constants/AttributeConstants.java
@@ -0,0 +1,41 @@
+package org.maxim.issuetracker.web.constants;
+
+public final class AttributeConstants {
+
+    public static final String PARAM_NEW = "new";
+    public static final String PARAM_ID = "id";
+    public static final String PARAM_PROJECT = "project";
+    public static final String PARAM_CREATE = "create";
+    public static final String PARAM_EXPORT = "export";
+    public static final String PARAM_REPORT = "report";
+    public static final String PARAM_SEARCH = "search";
+
+
+
+    public static final String ATTR_POSITIONS = "positions";
+    public static final String ATTR_NEW_PROJECT = "newProject";
+    public static final String ATTR_ERROR_MSG = "errorMessage";
+    public static final String ATTR_LOGOUT_MSG = "logoutMessage";
+    public static final String ATTR_PROJECT = "project";
+    public static final String ATTR_ISSUES = "issues";
+    public static final String ATTR_ADD_MEMBER = "addMember";
+    public static final String ATTR_EMPLOYEES = "employees";
+    public static final String ATTR_ROLES = "roles";
+    public static final String ATTR_LAST_ACTIVITIES = "lastActivities";
+    public static final String ATTR_ASSIGN_TO_ME = "assignToMe";
+    public static final String ATTR_ASSIGN = "assign";
+    public static final String ATTR_MEMBERS = "members";
+    public static final String ATTR_USER = "user";
+    public static final String ATTR_NEW_ASSIGN = "newAssign";
+    public static final String ATTR_NEW_REPORT = "newReport";
+    public static final String ATTR_ALL_PROJECTS = "allProjects";
+
+
+
+    public static final String SUCCESS_RESPONSE_BODY = "";
+
+
+
+    private AttributeConstants() { }
+
+}
