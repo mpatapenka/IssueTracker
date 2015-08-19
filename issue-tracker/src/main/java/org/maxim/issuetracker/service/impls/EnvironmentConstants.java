f8e7b0fc2271eb3cde06062f9a9d3f94d3d3a828
Maxim Potapenko <maxim131994@gmail.com>
2015-08-20 01:05:56 +0300

    Added new constants.

---
+++ issue-tracker/src/main/java/org/maxim/issuetracker/service/impls/EnvironmentConstants.java
@@ -0,0 +1,13 @@
+package org.maxim.issuetracker.service.impls;
+
+final class EnvironmentConstants {
+
+    public static final String FILES_PATH = "C:/issue_tracker/resources/";
+
+    public static final String RESPONSE_CONTENT_TYPE = "application/octet-stream";
+    public static final String RESPONSE_HEADER_NAME = "Content-Disposition";
+    public static final String RESPONSE_HEADER_SUFFIX = "attachment; filename=\"";
+
+    private EnvironmentConstants() { }
+
+}
