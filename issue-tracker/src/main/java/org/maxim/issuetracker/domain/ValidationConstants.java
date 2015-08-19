6694dc36ecff6c65c17ce5f97f8334378e234b6f
Maxim Potapenko <maxim131994@gmail.com>
2015-08-20 01:11:50 +0300

    Changed min duration boundary.

--- issue-tracker/src/main/java/org/maxim/issuetracker/domain/ValidationConstants.java
+++ issue-tracker/src/main/java/org/maxim/issuetracker/domain/ValidationConstants.java
@@ -2,7 +2,7 @@ package org.maxim.issuetracker.domain;
 
 final class ValidationConstants {
 
-    public static final int MIN_DURATION = 0;
+    public static final int MIN_DURATION = 5;
     public static final int MAX_DURATION_PER_DAY = 1440;
     public static final int TEXT_MAX_SIZE = 255;
 
