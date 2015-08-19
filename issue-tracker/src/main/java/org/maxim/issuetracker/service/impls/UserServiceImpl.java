872df6167f1226d3dfa8d95b9c4ef0f842d5a335
Maxim Potapenko <maxim131994@gmail.com>
2015-08-20 01:17:08 +0300

    Added realisation for methods upload/download files and changing issue status.

--- issue-tracker/src/main/java/org/maxim/issuetracker/service/impls/UserServiceImpl.java
+++ issue-tracker/src/main/java/org/maxim/issuetracker/service/impls/UserServiceImpl.java
@@ -1,15 +1,22 @@
 package org.maxim.issuetracker.service.impls;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.maxim.issuetracker.dao.*;
 import org.maxim.issuetracker.domain.*;
+import org.maxim.issuetracker.security.SecurityConstants;
 import org.maxim.issuetracker.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.sql.Date;
 import java.util.Calendar;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -32,6 +39,9 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private ActivityDAO activityDAO;
+
+    @Autowired
+    private AttachmentDAO attachmentDAO;
 
     @Override
     @Transactional
@@ -102,6 +112,107 @@ public class UserServiceImpl implements UserService {
 
         origin.setDescription(assigment.getDescription());
         origin.setMember(assignee);
+    }
+
+    @Override
+    @Transactional
+    public void transferIssue(int assigmentId, String username, String statusParam) {
+        Employee employee = employeeDAO.findByLogin(username);
+        Assigment assigment = assigmentDAO.findById(assigmentId);
+        List<Status> statuses = statusDAO.list();
+
+        if (statuses.indexOf(assigment.getTask().getStatus()) == statuses.size() - 1) {
+            return;
+        }
+
+        String currentPos = employee.getPosition().getName();
+        boolean isLead = SecurityConstants.TEAM_LEAD_POSITION.equals(currentPos)
+                || SecurityConstants.PROJECT_MANAGER_POSITION.equals(currentPos);
+
+        boolean isAssign = employee.getMembers().contains(assigment.getMember());
+
+        Set<Member> userMembers = new HashSet<>(employee.getMembers());
+        Set<Member> projectMembers = new HashSet<>(assigment.getTask().getProject().getMembers());
+        userMembers.retainAll(projectMembers);
+        boolean isProjectMember = !userMembers.isEmpty();
+
+        final int startProgress = 1;
+        final int delivery = 2;
+        final int done = 3;
+
+        Task task = assigment.getTask();
+
+        int prevPos = statuses.indexOf(task.getStatus());
+
+        switch (statusParam) {
+            case "start-progress":
+                if (isAssign && prevPos == startProgress - 1) {
+                    task.setStatus(statuses.get(startProgress));
+                    task.setActionStartDate(new Date(Calendar.getInstance().getTime().getTime()));
+                }
+                break;
+            case "delivery":
+                if (isAssign && prevPos == delivery - 1) {
+                    task.setStatus(statuses.get(delivery));
+                }
+                break;
+            case "done":
+                if (isProjectMember && isLead && prevPos == done - 1) {
+                    task.setStatus(statuses.get(done));
+                    task.setActionEndDate(new Date(Calendar.getInstance().getTime().getTime()));
+                }
+                break;
+            case "reject":
+                if (isProjectMember && isLead && prevPos == delivery) {
+                    task.setStatus(statuses.get(startProgress));
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported option '" + statusParam + "'.");
+        }
+    }
+
+    @Override
+    @Transactional
+    public void attachFile(int assigmentId, Attachment attachment, MultipartFile file) {
+        Assigment assigment = assigmentDAO.findById(assigmentId);
+
+        String filename = file.getOriginalFilename();
+        String sysFilename = Calendar.getInstance().getTime().getTime() + filename;
+
+        attachment.setTask(assigment.getTask());
+        attachment.setProject(assigment.getTask().getProject());
+        attachment.setName(filename);
+        attachment.setSize(file.getSize());
+        attachment.setFileSystemName(sysFilename);
+
+        attachmentDAO.save(attachment);
+
+        try {
+            File saveFile = new File(EnvironmentConstants.FILES_PATH + sysFilename);
+            FileUtils.writeByteArrayToFile(saveFile, file.getBytes());
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Can't upload file.", e);
+        }
+    }
+
+    @Override
+    @Transactional
+    public void downloadFile(int attachmentId, HttpServletResponse response) {
+        Attachment attachment = attachmentDAO.findById(attachmentId);
+        File file = new File(EnvironmentConstants.FILES_PATH + attachment.getFileSystemName());
+
+        response.setContentType(EnvironmentConstants.RESPONSE_CONTENT_TYPE);
+        response.setHeader(EnvironmentConstants.RESPONSE_HEADER_NAME,
+                EnvironmentConstants.RESPONSE_HEADER_SUFFIX + attachment.getName() + "\"");
+
+        try (InputStream is = new FileInputStream(file);
+             OutputStream os = response.getOutputStream()) {
+            IOUtils.copy(is, os);
+            response.flushBuffer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
