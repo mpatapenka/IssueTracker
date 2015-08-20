3e7d892d856c39f8dec0d98aa99d1c9509d09804
Maxim Potapenko <maxim131994@gmail.com>
2015-08-20 03:10:23 +0300

    Added message constants.

---
+++ issue-tracker/src/main/java/org/maxim/issuetracker/web/constants/MessageConstants.java
@@ -0,0 +1,17 @@
+package org.maxim.issuetracker.web.constants;
+
+public final class MessageConstants {
+
+    public static final String INVALID_FORM_DATA = "Check your input data. Some values was incorrect.";
+    public static final String LOGIN_ERROR = "Username or password is incorrect.";
+    public static final String LOGOUT_SUCCESS = "Logout successful.";
+    public static final String EMPLOYEE_ALREADY_ASSOCIATED_WITH_PROJECT = "Employee already working with project.";
+    public static final String PROJECT_NOT_CHOSEN = "You didn't choose a project.";
+    public static final String REQUEST_WAS_MODIFIED = "Your request was modified before send.";
+    public static final String USER_NOT_PROJECT_MEMBER = "Sorry, you are not a member of the current project.";
+    public static final String MEMBER_NOT_CHOSEN = "Sorry, you didn't select a member of the project.";
+    public static final String CANT_UPLOAD_FILE = "Can't upload file.";
+
+    private MessageConstants() { }
+
+}
